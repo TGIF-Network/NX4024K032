db137785cb0b6eedee5ad3720192a5d7b891a4e2
KF6S <43057968+KF6S@users.noreply.github.com>
2020-03-10 23:02:32 -0700

    Update cpyfiles.sh

--- cpyfiles.sh
+++ cpyfiles.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
 #############################################################
 #  Nextion Support for Nextion screen. Used to copy         #
 #  files from correct screen model repository to the        #
@@ -7,7 +6,6 @@
 #                                                           #
 #  KF6S                                         03-10-2020  #
 #############################################################
-
 sudo mount -o remount,rw /
 sudo mkdir /usr/local/etc/Nextion_Support
 sudo cp /home/pi-star/Nextion_Temp/{*.sh,*.fw} /usr/local/etc/Nextion_Support
