a0266809ebd7853f6e77552cd65d63ffff38fdd7
KF6S <43057968+KF6S@users.noreply.github.com>
2020-03-10 23:03:38 -0700

    Update cpyfiles.sh

--- cpyfiles.sh
+++ cpyfiles.sh
@@ -6,6 +6,7 @@
 #                                                           #
 #  KF6S                                         03-10-2020  #
 #############################################################
+
 sudo mount -o remount,rw /
 sudo mkdir /usr/local/etc/Nextion_Support
 sudo cp /home/pi-star/Nextion_Temp/{*.sh,*.fw} /usr/local/etc/Nextion_Support
