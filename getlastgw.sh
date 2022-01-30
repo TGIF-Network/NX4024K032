4c3461788a578e1f3c460b859979bfa34d70df9b
Philip Thompson <43919012+VE3RD@users.noreply.github.com>
2022-01-29 22:34:49 -0500

    Update getlastgw.sh

--- getlastgw.sh
+++ getlastgw.sh
@@ -14,19 +14,19 @@ FILE=/var/log/pi-star/DMRGateway-"$td".log
 Addr=$(sed -nr "/^\[DMR Network\]/ { :l /^Address[ ]*=/ { s/.*=[ ]*//; p; q;}; n; b l;}" /etc/mmdvmhost)
 
 
-        if [ $Addr = "127.0.0.1" ] && [[ -f "$FILE" ]]; then
+        if [ $Addr = "127.0.0.1" ]; then
                 GW="ON"
 		f1=$(ls /var/log/pi-star/DMRGateway* | tail -n1)
                 NetType=$(sudo tail -n1 "$f1" | cut -d " " -f 4)
 		NetNum=$(sudo tail -n1 "$f1" | cut -d " " -f 6)
 		NName=$(sed -nr "/^\[DMR Network "${NetNum##*( )}"\]/ { :l /^Name[ ]*=/ { s/.*=[ ]*//; p; q;}; n; b l;}" /etc/dmrgateway)
-#		echo "${NName##*( )}"
+
 		if [ "$NetType" = "RFRX" ]; then
    			echo "N:""${NetNum##*( )}"" $NName"
 		else
 			echo "N:""${NetNum##*( )}"" $NName"
 		fi
-#echo "$NetType"
+
         else
                 GW="OFF"
                 ms=$(sudo sed -n '/^[^#]*'"$Addr"'/p' /usr/local/etc/DMR_Hosts.txt | sed -E "s/[[:space:]]+/|/g" | cut -d'|' -f1)
