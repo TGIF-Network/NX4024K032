73fc735d56b619c355b9d0b6a676676983449c19
Philip Thompson <43919012+VE3RD@users.noreply.github.com>
2022-02-14 15:39:44 -0500

    Add files via upload

--- getlastgw.sh
+++ getlastgw.sh
@@ -8,31 +8,52 @@
 set -o errexit
 set -o pipefail
 
-td=$(date "+%Y-%m-%d")
-FILE=/var/log/pi-star/DMRGateway-"$td".log
-#echo "File::$FILE"
 Addr=$(sed -nr "/^\[DMR Network\]/ { :l /^Address[ ]*=/ { s/.*=[ ]*//; p; q;}; n; b l;}" /etc/mmdvmhost)
+f1=$(ls  /var/log/pi-star/DMRGateway* | tail -n1)
+f2=$(ls  /var/log/pi-star/MMDVM* | tail -n1)
 
+nMode=$(tail -n1 "$f2" | cut -d " " -f 4 | cut -d "," -f 1)
 
-        if [ $Addr = "127.0.0.1" ]; then
-                GW="ON"
-		f1=$(ls /var/log/pi-star/DMRGateway* | tail -n1)
-                NetType=$(sudo tail -n1 "$f1" | cut -d " " -f 4)
-		NetNum=$(sudo tail -n1 "$f1" | cut -d " " -f 6)
-		NName=$(sed -nr "/^\[DMR Network "${NetNum##*( )}"\]/ { :l /^Name[ ]*=/ { s/.*=[ ]*//; p; q;}; n; b l;}" /etc/dmrgateway)
+#echo "$f1"
+#echo "$f2"
+#echo "$nMode"
+        if [ "$nMode" == "DMR" ]; then
+        	if [ $Addr = "127.0.0.1" ]; then
+                	GW="ON"
+                	#fn=$(ls /var/log/pi-star/DMRGateway* | tail -n1 )
+                	NetType=$(sudo tail -n1 "$f1" | cut -d " " -f 4)
+      #          	NetNum=$(sudo tail -n1 /var/log/pi-star/DMRG* | cut -d " " -f 6)
+			NetNum=$(sudo tail -n1 "$f1" | cut -d " " -f 6)
+			NName=$(sed -nr "/^\[DMR Network "${NetNum##*( )}"\]/ { :l /^Name[ ]*=/ { s/.*=[ ]*//; p; q;}; n; b l;}" /etc/dmrgateway)
+			NName=$(echo "$NName" | cut -d "_" -f1 |  tr '[:lower:]' '[:upper:]')
 
-		if [ "$NetType" = "RFRX" ]; then
-   			echo "N:""${NetNum##*( )}"" $NName"
+#				echo "${NName##*( )}"
+				#if [ "$NetType" = "RFRX" ]; then
+#   			echo "$nMode GW:""${NetNum##*( )}"" $NName"
+   			echo "DMR|$NName|GW:${NetNum##*( )}"
+				#else
+			#	echo "$nMode GW:""${NetNum##*( )}"" $NName"
 		else
-			echo "N:""${NetNum##*( )}"" $NName"
+        
+                	GW="OFF"
+                	ms=$(sudo sed -n '/^[^#]*'"$Addr"'/p' /usr/local/etc/DMR_Hosts.txt | sed -E "s/[[:space:]]+/|/g" | cut -d'|' -f1)
+                	echo "DMR|$ms"
 		fi
+#		echo "Mode=DMR"
+	fi
 
-        else
-                GW="OFF"
-                ms=$(sudo sed -n '/^[^#]*'"$Addr"'/p' /usr/local/etc/DMR_Hosts.txt | sed -E "s/[[:space:]]+/|/g" | cut -d'|' -f1)
-                echo "MS:$ms"
-        fi
+        if [ "$nMode" == "P25" ]; then
+#		echo "Mode=P25"
+		tg=$(sed -nr "/^\[Network\]/ { :l /^Static[ ]*=/ { s/.*=[ ]*//; p; q;}; n; b l;}" /etc/p25gateway)
+		server=$(grep 10210 /usr/local/etc/P25HostsLocal.txt |  tr '\t' ' ' | cut -d " " -f2 | cut -d "." -f1 | tr '[:lower:]' '[:upper:]')
+		echo "P25|$server"
+	fi
 
+        if [ "$nMode" == "YSF" ]; then
+		echo "YSF|N/A"
+	fi
 
-
+        if [ "$nMode" == "NXDN" ]; then
+		echo "NXDN|N/A"
+	fi
 
